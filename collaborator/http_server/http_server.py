e23e10a00006b83d8672c93134182f53a6f4dd9f
Quentin Laporte-Chabasse <quentin.laporte-chabasse@inria.fr>
2017-04-20 14:12:08 +0200

    [+] basic C&C HTTP server

---
+++ collaborator/http_server/http_server.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify
+from flask.ext.api import status
+from collaborator.controller import Controller
+import netifaces as ni
+
+controller = Controller('example.ini')
+app = Flask(__name__)
+
+DEFAULT_PORT = 5000
+
+
+@app.route('/get/status', methods=['GET'])
+def getStatus():
+    return jsonify(controller.getStatus()), status.HTTP_200_OK
+
+
+@app.route('/get/mute/collaborator/status', methods=['GET'])
+def getMuteCollaboratorStatus():
+    pass
+
+
+@app.route('/create/mute/collaborator', methods=['POST'])
+def createMuteCollaborator():
+    return jsonify(controller.createMuteCollaborator()), status.HTTP_200_OK
+
+
+@app.route('/start/mute/collaborator', methods=['PUT'])
+def startMuteCollaborator():
+    return jsonify(controller.startMuteCollaborator()), status.HTTP_200_OK
+
+
+@app.route('/stop-writing/mute/collaborator', methods=['PUT'])
+def stopWritingMuteCollaborator():
+    return jsonify(controller.stopWritingMuteCollaborator())
+
+
+@app.route('/stop-reading/mute/collaborator', methods=['PUT'])
+def stopReadingMuteCollaborator():
+    return jsonify(controller.stopReadingMuteCollaborator())
+
+
+@app.route('/retrieve/mute/collaborator/records', methods=['GET'])
+def retrieveMuteCollabRecords():
+    return jsonify({'status': 'Not yet implemented'})
+
+
+def entryPoint():
+    config = controller.getConfig()
+
+    if not config:
+        config['port'] = DEFAULT_PORT
+
+    if 'address' not in config:
+        # Get the right interface
+        gws = ni.gateways()
+        interface = gws['default'][2][1]
+
+        ni.ifaddresses(interface)
+        ip_addr = ni.ifaddresses(interface)[2][0]['addr']
+        config['address'] = ip_addr
+
+    app.run(port=config['port'], host=config['address'])
