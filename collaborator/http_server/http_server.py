0cc9530348500d5df4c6cdc228d15fdafcf5ceb8
Quentin Laporte-Chabasse <quentin.laporte-chabasse@inria.fr>
2017-04-21 17:16:46 +0200

    [F] bug related to config file path

--- collaborator/http_server/http_server.py
+++ collaborator/http_server/http_server.py
@@ -1,53 +1,47 @@
 from flask import Flask, jsonify
-from flask.ext.api import status
+from flask_api import status
 from collaborator.controller import Controller
 import netifaces as ni
 
-controller = Controller('example.ini')
 app = Flask(__name__)
 
 DEFAULT_PORT = 5000
 
 
-@app.route('/get/status', methods=['GET'])
-def getStatus():
-    return jsonify(controller.getStatus()), status.HTTP_200_OK
+def entryPoint(path_to_config):
 
+    @app.route('/get/status', methods=['GET'])
+    def getStatus():
+        return jsonify(controller.getStatus()), status.HTTP_200_OK
 
-@app.route('/get/mute/collaborator/status', methods=['GET'])
-def getMuteCollaboratorStatus():
-    pass
+    @app.route('/get/mute/collaborator/status', methods=['GET'])
+    def getMuteCollaboratorStatus():
+        pass
 
+    @app.route('/create/mute/collaborator', methods=['POST'])
+    def createMuteCollaborator():
+        return jsonify(controller.createMuteCollaborator()), status.HTTP_200_OK
 
-@app.route('/create/mute/collaborator', methods=['POST'])
-def createMuteCollaborator():
-    return jsonify(controller.createMuteCollaborator()), status.HTTP_200_OK
+    @app.route('/start/mute/collaborator', methods=['PUT'])
+    def startMuteCollaborator():
+        return jsonify(controller.startMuteCollaborator()), status.HTTP_200_OK
 
+    @app.route('/stop-writing/mute/collaborator', methods=['PUT'])
+    def stopWritingMuteCollaborator():
+        return jsonify(controller.stopWritingMuteCollaborator())
 
-@app.route('/start/mute/collaborator', methods=['PUT'])
-def startMuteCollaborator():
-    return jsonify(controller.startMuteCollaborator()), status.HTTP_200_OK
+    @app.route('/stop-reading/mute/collaborator', methods=['PUT'])
+    def stopReadingMuteCollaborator():
+        return jsonify(controller.stopReadingMuteCollaborator())
 
+    @app.route('/retrieve/mute/collaborator/records', methods=['GET'])
+    def retrieveMuteCollabRecords():
+        return jsonify({'status': 'Not yet implemented'})
 
-@app.route('/stop-writing/mute/collaborator', methods=['PUT'])
-def stopWritingMuteCollaborator():
-    return jsonify(controller.stopWritingMuteCollaborator())
-
-
-@app.route('/stop-reading/mute/collaborator', methods=['PUT'])
-def stopReadingMuteCollaborator():
-    return jsonify(controller.stopReadingMuteCollaborator())
-
-
-@app.route('/retrieve/mute/collaborator/records', methods=['GET'])
-def retrieveMuteCollabRecords():
-    return jsonify({'status': 'Not yet implemented'})
-
-
-def entryPoint():
+    controller = Controller(path_to_config)
     config = controller.getConfig()
 
-    if not config:
+    if not config or 'port' not in config:
         config['port'] = DEFAULT_PORT
 
     if 'address' not in config:
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 from collaborator.http_server.http_server import entryPoint
+import sys
+import os
 
 if __name__ == '__main__':
-    entryPoint()
+    if len(sys.argv) < 2:
+        path_to_config = os.getenv('CONFIG_PATH', './example.ini')
+    else:
+        path_to_config = sys.argv[1]
+
+    if not os.path.exists(path_to_config):
+        sys.exit("Path to config file : %s doesn't exist" % path_to_config)
+    entryPoint(path_to_config)
